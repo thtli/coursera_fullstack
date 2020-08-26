d5b0643a7065298a9a7c71b06d0960c24299f46c
tinahli <tinahuitingli@gmail.com>
2020-08-25 18:47:27 -0700

    Grunt Part 2

--- 1-frontend-bootstrap/Bootstrap4/conFusion/Gruntfile.js
+++ 1-frontend-bootstrap/Bootstrap4/conFusion/Gruntfile.js
@@ -1,11 +1,15 @@
 'use strict';
 
 module.exports = function(grunt) {
-
+    // time how long tasks take. can help when optimizing build times. 
     require('time-grunt')(grunt);
 
-    require('jit-grunt')(grunt);
+    // automatically load required Grunt tasks
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
+    // define the configuration for all the tasks
     grunt.initConfig({
         sass: {
             dist: {
@@ -14,10 +18,12 @@ module.exports = function(grunt) {
                 }
             }
         },
+
         watch: {
             files: 'css/*.scss', 
             tasks: ['sass']
         },
+
         browserSync: {
             dev: {
                 bsFiles: {
@@ -33,9 +39,152 @@ module.exports = function(grunt) {
                     }
                 }
             }
+        },
+
+        copy: {
+            html: {
+                files: [{
+                    // for html
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    // for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true, // enable dynamic expansion
+                    cwd: './',   // src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'], // actual patterns to match
+                    dest: 'dist/' // destination path prefix
+                }]
+            }
+        },
+
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html'] // only index.html cause usemin take care other files
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+
+        // concat
+        concat: {
+            options: {
+                separator: ';'
+            },
+
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        // uglify
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        cssmin: {
+            dist: {}
+        },
+
+        // filerev
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+
+            release: {
+                // filerev:release hashes(md5) all assets (images, js and css)
+                // in dist directory
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        
+        // usemin
+        // replaces all assets with their revved version in html and css files
+        // options.assetsDirs contins the directories for finding the assets
+        // according to their relative paths 
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        }, 
+
+        htmlmin: { // task
+            dist: { // target
+                options: { // target options
+                    collapseWhitespace: true,
+                },
+                files: { // dictionary of files 
+                    // 'destination':'source'
+                    'dist/index.html': 'dist/index.html',
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html',
+                }
+            }
         }
     });
 
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ])
 };
