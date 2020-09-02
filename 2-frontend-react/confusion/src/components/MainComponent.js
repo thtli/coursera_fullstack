6250e1067f664b2389033f755aaf2080b5c5b574
tinahli <tinahuitingli@gmail.com>
2020-09-01 23:24:26 -0700

    React Router

--- 2-frontend-react/confusion/src/components/MainComponent.js
+++ 2-frontend-react/confusion/src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   
@@ -16,21 +18,24 @@ class Main extends Component {
         };
     }
       
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
 
     render() {
+        
+        const HomePage = () => {
+            return (
+                <Home/>
+            );
+        }
+
+        // menu needs to be specified that way so that it can receive props
         return (
             <div>
                 <Header/>
-                <Menu 
-                    dishes={this.state.dishes} 
-                    onClick={(dishId) => this.onDishSelect(dishId)}
-                />
-                <Dishdetail 
-                    selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}
-                />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}></Route>
+                    <Redirect to="/home"/>
+                </Switch>
                 <Footer/>
             </div>
         );
