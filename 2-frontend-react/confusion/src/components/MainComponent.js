f9bde168d927b2a28efef169cf0017e5a534fff7
tinahli <tinahuitingli@gmail.com>
2020-09-02 23:01:51 -0700

    Single Page Applications Part 2

--- 2-frontend-react/confusion/src/components/MainComponent.js
+++ 2-frontend-react/confusion/src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -35,6 +36,16 @@ class Main extends Component {
             );
         }
 
+        // link passes 3 params (match, location, history) but we extract only match
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail 
+                    dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} 
+                />
+            );
+        }
+
         // menu needs to be specified that way so that it can receive props
         return (
             <div>
@@ -43,6 +54,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}></Route>
                     <Route exact path="/contactus" component={Contact} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Redirect to="/home"/>
                 </Switch>
                 <Footer/>
