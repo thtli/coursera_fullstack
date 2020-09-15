785a46326c0966d888632c5981f7ee6ce176cfa6
tinahli <tinahuitingli@gmail.com>
2020-09-14 21:37:19 -0700

    Combining Reducers

--- 2-frontend-react/confusion/src/redux/configureStore.js
+++ 2-frontend-react/confusion/src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        Reducer, // reducer
-        initialState // our initialState
+    const store = createStore( //combine reducers that manage partial states into one
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
