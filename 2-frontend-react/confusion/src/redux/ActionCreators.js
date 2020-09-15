7635374bb32fc5b8cd7d6a0f6ce909ad310467c5
tinahli <tinahuitingli@gmail.com>
2020-09-14 23:43:49 -0700

    Redux Thunk

--- 2-frontend-react/confusion/src/redux/ActionCreators.js
+++ 2-frontend-react/confusion/src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
 // action type object
 export const addComment = (dishId, rating, author, comment) => ({
@@ -10,3 +11,27 @@ export const addComment = (dishId, rating, author, comment) => ({
         comment: comment
     }
 });
+
+// a thunk, returns a function, dispatch is inner function, defined in {}
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES)); // will push dishes into state 
+    }, 2000);
+}
+
+// return action type
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes 
+})
