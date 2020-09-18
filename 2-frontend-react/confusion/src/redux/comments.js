95b5327ac8ed174086cd2bde4f7524d4b1c28565
tinahli <tinahuitingli@gmail.com>
2020-09-17 23:32:37 -0700

    Fetch from Server

--- 2-frontend-react/confusion/src/redux/comments.js
+++ 2-frontend-react/confusion/src/redux/comments.js
@@ -1,13 +1,22 @@
-import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
-export const Comments = (state = COMMENTS, action ) => {
+export const Comments = (state = {
+        errMess: null,
+        comments: []
+    }, action ) => {
     switch(action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, errMess: null, comments: action.payload};
+
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, errMess: action.payload, comments: []};
+
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
-            comment.id = state.length; //assign an id in seq order based on existing comments
+            comment.id = state.comments.length; //assign an id in seq order based on existing comments
             comment.date = new Date().toISOString();
-            return state.concat(comment); //create new object with added comment
+            return { ...state, comments: state.comments.concat(comment) }; //create new object with added comment
+        
         default: 
             return state;
     }
--- 2-frontend-react/confusion/src/redux/promotions.js
+++ 2-frontend-react/confusion/src/redux/promotions.js
@@ -1,7 +1,20 @@
-import { PROMOTIONS } from '../shared/promotions';
+import * as ActionTypes from './ActionTypes';
 
-export const Promotions = (state = PROMOTIONS, action ) => {
+export const Promotions = (state = {
+        isLoading: true,
+        errMess: null,
+        promotions: []
+    }, action ) => {
     switch(action.type) {
+        case ActionTypes.ADD_PROMOS:
+            return {...state, isLoading: false, errMess: null, promotions: action.payload};
+
+        case ActionTypes.PROMOS_LOADING:
+            return {...state, isLoading: true, errMess: null, promotions: []}; //take same state and whatever else passed in as modification
+
+        case ActionTypes.PROMOS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload, promotions: []};
+
         default: 
             return state;
     }
