cc35b0d348d1130fcd1715534b4b0aae5c60a17f
tinahli <tinahuitingli@gmail.com>
2020-10-01 21:57:39 -0700

    Navigation Part 2 - with fixes for using new versions of Drawer Navigator

--- 3-mobileapp-react-native/confusion/components/MainComponent.js
+++ 3-mobileapp-react-native/confusion/components/MainComponent.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { View, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import Menu from './MenuComponent';
 import Dishdetail from './DishDetailComponent';
+import Home from './HomeComponent';
 
 
 const MenuNavigator = createStackNavigator()
@@ -35,12 +37,58 @@ function MenuNavigatorScreen() {
     );
 }
 
+const HomeNavigator = createStackNavigator();
+
+function HomeNavigatorScreen() {
+    return(
+        <HomeNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <HomeNavigator.Screen 
+                name='Home'
+                component={Home}
+            />
+        </HomeNavigator.Navigator>
+    );
+} 
+
+const MainNavigator = createDrawerNavigator();
+
+function MainNavigatorDrawer(){
+    return(
+        <MainNavigator.Navigator
+            initialRouteName='Home'
+            drawerStyle={{
+                backgroundColor: '#D1C4E9'
+            }}
+        >
+            <MainNavigator.Screen 
+                name='Home'
+                component={HomeNavigatorScreen}
+            />
+            <MainNavigator.Screen 
+                name='Menu'
+                component={MenuNavigatorScreen}
+            />
+        </MainNavigator.Navigator>
+    );
+}
+
 class Main extends Component {
 
   render() {
     return (
         <NavigationContainer>
-            <MenuNavigatorScreen />
+            <MainNavigatorDrawer />
         </NavigationContainer>
     );
   }
