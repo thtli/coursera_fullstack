da00d6f06b905b7c553a0032e5664a3c830e28be
tinahli <tinahuitingli@gmail.com>
2020-10-01 20:16:56 -0700

    Navigation Part 1 - with fixes for using new versions of Stack Navigator

--- 3-mobileapp-react-native/confusion/components/MainComponent.js
+++ 3-mobileapp-react-native/confusion/components/MainComponent.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import DishDetail from './DishDetailComponent';
+import Dishdetail from './DishDetailComponent';
 
+
+const MenuNavigator = createStackNavigator()
+
+function MenuNavigatorScreen() {
+    return(
+        <MenuNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={Menu}
+            />
+            <MenuNavigator.Screen
+                name="Dishdetail"
+                component={Dishdetail}
+                options={{ headerTitle: "Dish Detail"}}
+            />            
+        </MenuNavigator.Navigator>
+    );
+}
 
 class Main extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
-
-  onDishSelect(dishId) {
-      this.setState({selectedDish: dishId});
-  }
 
   render() {
     return (
-        <View>
-            <Menu 
-                dishes={this.state.dishes} 
-                onPress={(dishId) => this.onDishSelect(dishId)}
-            />
-            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />    
-        </View>
-        
+        <NavigationContainer>
+            <MenuNavigatorScreen />
+        </NavigationContainer>
     );
   }
 }
