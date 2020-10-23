2d3c3632524fd938b5859514a68303259b47ac68
tinahli <tinahuitingli@gmail.com>
2020-10-22 23:35:29 -0700

    Gestures Part 2

--- 3-mobileapp-react-native/confusion/components/DishDetailComponent.js
+++ 3-mobileapp-react-native/confusion/components/DishDetailComponent.js
@@ -23,6 +23,8 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish(props) {
     const dish = props.dish;
 
+    handleViewRef = ref => this.view = ref;
+
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if (dx < -200) // right to left drag
             return true;
@@ -33,6 +35,10 @@ function RenderDish(props) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
+        },
+        onPanResponderGrant: () => { //animation 
+            this.view.rubberBand(1000)
+                .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
         },
         onPanResponderEnd: (e, gestureState) => {
             if (recognizeDrag(gestureState))
@@ -59,6 +65,7 @@ function RenderDish(props) {
     if (dish != null) {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+                ref={this.handleViewRef}
                 {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
